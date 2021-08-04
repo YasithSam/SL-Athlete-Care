7e1b4845dbed9d262db283f86ed5e17343fe6cd1
Yashodha Subhasinghe <yashodasubasinghe19@gmail.com>
2021-08-04 20:54:35 +0530

    Services and About

---
+++ application/views/landing/about.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>About Us</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="../../../public/assets/css/Landing/about.css">
+     
+  </head>
+  <body>
+
+<div class="imagebox">
+    <img src="../../../public/assets/img/about.png" alt="" width="90%" height="auto">
+</div>
+    
+<div class="textbox-cont">
+    <div class="textbox">
+        <div class="main">Leading healthcare providers</div>
+        <hr class="main_line"/> <br>
+        <div class="para">SL Athlete Care provides progressive, and affordable health care, accessible on mobile and online for all the athletes. To us, it's not just work. We take pride in the solutions we deliver.
+        </div>
+        <a href="#" class="main-btn">Contact Us</a>
+             
+
+
+    </div>
+</div>
+
+     </body>
+     </html>
---
+++ application/views/landing/services.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Services</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../../../public/assets/css/Landing/services.css">
+</head>
+<body>
+ 
+<div class="wrapper">
+    <div class="header">
+        <div class="main">Our Services</div>
+        <hr class="main_line">
+    <div class="para">Expert doctors in the field of sports medicine are ready to serve the nation's athletes to thrive for their best </div>
+    </div>
+    <div class="cards_wrap">
+        <div class="card_item">
+            <div class="card_inner">
+                <img src="../../../public/assets/img/1366548.png" alt="icon">
+                <div class="title_name">Search Doctor</div>
+                <div class="content_part">Choose your doctor from the best in the field</div>
+            </div>
+        </div>
+
+        <div class="card_item">
+            <div class="card_inner">
+                <img src="../../../public/assets/img/2643830.png" alt="icon">
+                <div class="title_name">24/7 Service</div>
+                <div class="content_part">Choose your doctor from the best in the field</div>
+            </div>
+        </div>
+
+        
+        <div class="card_item">
+            <div class="card_inner">
+                <img src="../../../public/assets/img/2329018.png" alt="icon">
+                <div class="title_name">Consultation</div>
+                <div class="content_part">Choose your doctor from the best in the field</div>
+            </div>
+        </div>
+
+        <div class="card_item">
+            <div class="card_inner">
+                <img src="../../../public/assets/img/1934415.png" alt="icon">
+                <div class="title_name">Paramedical Services</div>
+                <div class="content_part">Choose your doctor from the best in the field</div>
+            </div>
+        </div>
+
+
+        <div class="card_item">
+            <div class="card_inner">
+                <img src="../../../public/assets/img/2463958.png" alt="icon">
+                <div class="title_name">Emergency Care</div>
+                <div class="content_part">Choose your doctor from the best in the field</div>
+            </div>
+        </div>
+
+        
+
+        <div class="card_item">
+            <div class="card_inner">
+                <img src="../../../public/assets/img/4835988.png" alt="icon">
+                <div class="title_name">Tracking</div>
+                <div class="content_part">Choose your doctor from the best in the field</div>
+            </div>
+        </div>
+    </div>
+
+    
+    <div class="end-button">
+        <a href="#" class="end-btn">Learn More</a>
+    </div>
+
+</div>
+
+</body>
+</html>
