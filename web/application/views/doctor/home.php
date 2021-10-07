4ee63dea78a24dceac4ba031e0636a52051cd46b
YasithSam <yasith.sam7@gmail.com>
2021-10-08 03:11:09 +0530

    Case Study Model

--- web/application/views/doctor/home.php
+++ web/application/views/doctor/home.php
@@ -1,1 +1,53 @@
-<h1>yoyo im doctor</h1>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1">
+        <title></title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/lykmapipo/themify-icons@0.1.2/css/themify-icons.css">
+        <?php linkCSS("assets/css/doctordashboard.css") ?>
+        <?php linkCSS("assets/css/doctormain.css") ?>
+
+    </head>
+
+    <body>
+        <input type="checkbox" id="sidebar-toggle">
+        <?php include "sidebar.php";?>
+
+        <div class="main-content">
+
+           <?php include "header.php";?>
+
+            <main>
+            <section class="home-section">
+    <div class="home-content">
+      <?php include "top.php";?> 
+      <div class="forum">
+         <?php include "bodyforum.php";?>
+       
+        <div class="updates box">
+           <?php include "bodyupdates.php";?>
+           
+        </div>
+      </div>
+      </div>
+  </section>
+
+             
+            </main>
+
+        </div>
+
+        <script>
+   let sidebar = document.querySelector(".sidebar");
+let sidebarBtn = document.querySelector(".sidebarBtn");
+sidebarBtn.onclick = function() {
+  sidebar.classList.toggle("active");
+  if(sidebar.classList.contains("active")){
+  sidebarBtn.classList.replace("bx-menu" ,"bx-menu-alt-right");
+}else
+  sidebarBtn.classList.replace("bx-menu-alt-right", "bx-menu");
+}
+ </script>
+    </body>
+</html>
