1e9257a80e8e4606192d9c1323701bd4875eb4b3
YasithSam <yasith.sam7@gmail.com>
2021-10-08 03:09:47 +0530

    API updated

--- web/application/models/injuryModel.php
+++ web/application/models/injuryModel.php
@@ -3,7 +3,7 @@
 class injuryModel extends database
  {
     public function getInjuryData(){
-        if($this->Query("SELECT i.injury,d.full_name,d.city FROM injury i , doctor_profile d")){
+        if($this->Query("SELECT i.injury,d.uuid,d.full_name,d.city FROM injury i , doctor_profile d")){
             $injury=[];
             $doctor=[];
             if($this->rowCount() > 0 ){
@@ -36,13 +36,32 @@ class injuryModel extends database
         }
 
      }
-     public function postInjury($athleteId,$injuryId,$date,$con,$d){
-        
-        if($this->Query("INSERT INTO athlete_reported_injury (athlete_id,injury_id,date,status,con,description) VALUES (?,?,?,?,?,?)",[$athleteId,$injuryId,$date,0,$con,$d])){
-            return true;
+     public function postInjury($athleteId,$injuryId,$doc,$con,$d){
+        $doctor=$this->findDoctor($doc);    
+        $injuryId=$this->findInjury($injuryId);
+
+        if($this->Query("INSERT INTO athlete_reported_injury(athlete_id,injury_id,status,doctor_id,con,description) VALUES (?,?,?,?,?,?)",[$athleteId,$injuryId,0,$doctor,$con,$d])){
+            return ['status' => 'ok'];
         }else{
             return false;
         }
      }
+     public function findDoctor($doc){
+        $x=explode('-',$doc);
+        $doctorId=$x[0];
+        if($this->Query("SELECT * FROM doctor_profile WHERE full_name=?",[$doctorId])){
+            $row=$this->fetch();
+          
+            return $row->uuid;
+        }
+     }
+     public function findInjury($injury){
+        if($this->Query("SELECT * FROM injury WHERE injury=?",[$injury])){
+            $r=$this->fetch();
+           
+            return $r->id;
+        }
+
+    }
 
 }
