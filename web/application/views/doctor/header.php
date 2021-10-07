566c3f68da2ef2fac337150b6358e862259dce15
YasithSam <yasith.sam7@gmail.com>
2021-10-08 03:12:15 +0530

    Case study items

---
+++ web/application/views/doctor/header.php
@@ -0,0 +1,18 @@
+<header>
+   <div class="register-wrapper">
+     <a href="3.html" target="myiFrame"><button>Register User <span class="ti-plus"></span></button></a>
+    </div>
+
+    <div class="social-icons">
+        <span class="ti-bell"></span>
+        <div class="user-wrapper">
+             <img src="avatar.jpg" width="40px" height="40px" alt="">
+             <div>
+                 <h4>Doctor</h4>
+                 <small>Doctor</small>
+            </div>                  
+        </div>                                  
+    </div>   
+
+              
+</header>
