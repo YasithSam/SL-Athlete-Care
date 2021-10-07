fcc35bf0c3fb2162007ad9e93f9c3f0d792108a3
YasithSam <yasith.sam7@gmail.com>
2021-10-08 03:12:52 +0530

    Patients

---
+++ web/application/views/doctor/sidebar.php
@@ -0,0 +1,62 @@
+<div class="sidebar">
+            <div class="sidebar-header">
+                <h3 class="brand">
+                    <span class="ti-unlink"></span>
+                    <span>SL Athlete Care</span>
+                </h3>
+                <label for="sidebar-toggle" class="ti-menu-alt"></label>
+            </div>
+
+            <div class="sidebar-menu">
+                <ul>
+                    <li id="db">
+                        <a href="<?php echo BASEURL;?>/doctor/dashboard">
+                            <span class="ti-dashboard"></span>
+                            <span>Dashboard</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="<?php echo BASEURL;?>/doctor/patients">
+                            <span class="ti-id-badge"></span>
+                            <span>Patients</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="<?php echo BASEURL;?>/doctor/casestudy">
+                            <span class="ti-files"></span>
+                            <span>Case Studies</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <span class="ti-comments"></span>
+                            <span>Messages</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <span class="ti-agenda"></span>
+                            <span>Forum</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <span class="ti-user"></span>
+                            <span>Profile</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <span class="ti-settings"></span>
+                            <span>Settings</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <span class="ti-power-off"></span>
+                            <span>Logout</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
