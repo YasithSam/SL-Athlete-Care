7546e95fd6fdd61b8873c930623ae6c089e97a6b
YasithSam <yasith.sam7@gmail.com>
2021-10-08 03:12:45 +0530

    Patients

---
+++ web/application/views/doctor/patientitems.php
@@ -0,0 +1,82 @@
+<div class="title">Patients Overview</div>
+                  <div class="pages">
+                      <i class="num">1</i>
+                      <i class="num">2</i>
+                      <i class="num">3</i>
+                      <i class="num">>></i>
+                  </div>
+                  <!--Injury card-->
+                  <div class="injury">
+                    <i class="fas fa-user user"></i>
+                    <div class="des1"><p>Name: </p><p>Sport: </p></div>
+                    <div class="des2"><p>A.B.C.Perera</p><p>Swimming</p></div>
+                    <div class="des3"></div>
+                    <div class="des1"><p>Injury: </p><p>Condition: </p></div>
+                    <div class="des2"><p>Arm strain</p><p>Mild</p></div>
+                    <div class="button">
+                      <ul style="list-style: none;">
+                      <li class="l1" style="padding-bottom: 5px;">Date: 01/02/2022</li>
+                      <li class="l2"><a href="#">View</a></li>
+                      </ul>
+                    </div>
+                  </div>
+                  <!--Injury card-->
+                  <div class="injury">
+                    <i class="fas fa-user user"></i>
+                    <div class="des1"><p>Name: </p><p>Sport: </p></div>
+                    <div class="des2"><p>A.B.C.Perera</p><p>Swimming</p></div>
+                    <div class="des3"></div>
+                    <div class="des1"><p>Injury: </p><p>Condition: </p></div>
+                    <div class="des2"><p>Arm strain</p><p>Mild</p></div>
+                    <div class="button">
+                      <ul style="list-style: none;">
+                      <li class="l1" style="padding-bottom: 5px;">Date: 01/02/2022</li>
+                      <li class="l2"><a href="#">View</a></li>
+                      </ul>
+                    </div>
+                  </div>
+                   <!--Injury card-->
+                   <div class="injury">
+                    <i class="fas fa-user user"></i>
+                    <div class="des1"><p>Name: </p><p>Sport: </p></div>
+                    <div class="des2"><p>A.B.C.Perera</p><p>Swimming</p></div>
+                    <div class="des3"></div>
+                    <div class="des1"><p>Injury: </p><p>Condition: </p></div>
+                    <div class="des2"><p>Arm strain</p><p>Mild</p></div>
+                    <div class="button">
+                      <ul style="list-style: none;">
+                      <li class="l1" style="padding-bottom: 5px;">Date: 01/02/2022</li>
+                      <li class="l2"><a href="#">View</a></li>
+                      </ul>
+                    </div>
+                  </div>
+                  <!--Injury card-->
+                  <div class="injury">
+                    <i class="fas fa-user user"></i>
+                    <div class="des1"><p>Name: </p><p>Sport: </p></div>
+                    <div class="des2"><p>A.B.C.Perera</p><p>Swimming</p></div>
+                    <div class="des3"></div>
+                    <div class="des1"><p>Injury: </p><p>Condition: </p></div>
+                    <div class="des2"><p>Arm strain</p><p>Mild</p></div>
+                    <div class="button">
+                      <ul style="list-style: none;">
+                      <li class="l1" style="padding-bottom: 5px;">Date: 01/02/2022</li>
+                      <li class="l2"><a href="#">View</a></li>
+                      </ul>
+                    </div>
+                  </div>
+                  <!--Injury card-->
+                  <div class="injury">
+                    <i class="fas fa-user user"></i>
+                    <div class="des1"><p>Name: </p><p>Sport: </p></div>
+                    <div class="des2"><p>A.B.C.Perera</p><p>Swimming</p></div>
+                    <div class="des3"></div>
+                    <div class="des1"><p>Injury: </p><p>Condition: </p></div>
+                    <div class="des2"><p>Arm strain</p><p>Mild</p></div>
+                    <div class="button">
+                      <ul style="list-style: none;">
+                      <li class="l1" style="padding-bottom: 5px;">Date: 01/02/2022</li>
+                      <li class="l2"><a href="#">View</a></li>
+                      </ul>
+                    </div>
+                  </div>
