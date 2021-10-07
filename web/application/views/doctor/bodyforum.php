e6bb3fd439126fa3329941c67b2934bd62432fa1
YasithSam <yasith.sam7@gmail.com>
2021-10-08 03:11:27 +0530

    Doctor component

---
+++ web/application/views/doctor/bodyforum.php
@@ -0,0 +1,26 @@
+<div class="forum-injury box">
+
+         
+          <div class="title">Forum Injuries</div>
+          <!--Injury card-->
+  
+            <div class="injury">
+              <i class="fas fa-user user"></i>
+              <div class="des1"><p>Name: </p><p>Sport: </p></div>
+              <div class="des2"><p>A.B.C.Perera</p><p>Swimming</p></div>
+              <div class="des3"></div>
+              <div class="des1-des11"><p>Injury: </p><p>Condition: </p></div>
+              <div class="des2"><p>Arm strain</p><p>Mild</p></div>
+              <div class="button">
+                <ul style="list-style: none;">
+                <li class="l1" style="padding-bottom: 5px;">Date: 01/02/2022</li>
+                <li class="l2"><a href="<?php echo BASEURL;?>/caseStudyController/item/1">View</a></li>
+                </ul>
+              </div>
+           
+          </div>
+         
+        
+
+
+        </div>
