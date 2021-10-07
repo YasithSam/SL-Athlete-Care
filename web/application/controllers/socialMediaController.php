e80b5b27ef69d959965fb3e8fe7f8ba770374903
YasithSam <yasith.sam7@gmail.com>
2021-10-08 03:10:47 +0530

    Case Study Model

---
+++ web/application/controllers/socialMediaController.php
@@ -0,0 +1,41 @@
+<?php
+
+class socialMediaController extends main{
+    public function __construct(){
+        if(!($this->getSession('userId'))){
+            $this->redirect("accountController");
+        }
+        $this->helper("link");
+    
+       $this->socialMediaModel = $this->model('socialMediaModel');
+
+    }
+    public function index(){
+        $this->view('socialmedia/index');
+    }
+    public function item($id){
+        // $userId = $this->getSession('userId');
+        // // check which user has access to the specific case study
+        // echo ($id);
+        // $this->caseStudyModel->access($id,$userId);
+
+        // if($this->getSession('userRole')==2){
+        //     $data = $this->doctorModel->getCounts($userId);   
+        //     //$data2= $this->doctorModel->getForumItems($userId);    
+        //     //$data3= $this->doctorModel->getUpdates($userId);   
+        //     //$dataArray=[$data,$data2,$data3];
+        //     $this->view("doctor/home",$data);
+              
+        // }
+        // else{
+        //     $this->view('404');
+        // }
+    }
+    
+    
+   
+   
+   
+
+}
+?>    
