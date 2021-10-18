62c8561be0e73821f65917bf3769eb673345ec41
YasithSam <yasith.sam7@gmail.com>
2021-10-18 14:02:32 +0530

    profile

---
+++ web/application/views/help.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/lykmapipo/themify-icons@0.1.2/css/themify-icons.css">
+     <?php linkCSS("assets/css/privacy.css") ?>
+     <?php linkCSS("assets/css/doctormain.css") ?>
+     <?php linkCSS("assets/css/doctordashboard.css") ?>
+   
+
+   </head>
+</head>
+<body>
+<?php include "doctor/header.php" ?>
+<?php include "doctor/sidebar.php"?>  
+
+<div class="pcontainer">
+  <h2>Help</h2>
+  <h4>Help section for users</h4>
+  <h6>This page informs you of our policies regarding the collection, use, and disclosure of personal data when you use our Service and the choices you have associated with that data. 
+      This Privacy Policy for SL Athlete CAre is powered by Sri Lanka Sports Medicine Association.
+      We use your data to provide and improve the Service. By using the Service, you agree to the collection and use of information in accordance with this policy.</h6>
+
+      <h4>Tracking & Cookies Data</h4>
+      <h6>We use cookies and similar tracking technologies to track the activity on our Service and hold certain information.
+          Cookies are files with small amount of data which may include an anonymous unique identifier. Cookies are sent to your browser from a website and stored on your device. Tracking technologies also used are beacons, tags, and scripts to collect and track information and to improve and analyze our Service.
+          You can instruct your browser to refuse all cookies or to indicate when a cookie is being sent. However, if you do not accept cookies, you may not be able to use some portions of our Service.</h6>
+
+      <h4>Changes To This Privacy Policy</h4>
+      <h6>We may update our Privacy Policy from time to time. We will notify you of any changes by posting the new Privacy Policy on this page.
+          We will let you know via email and/or a prominent notice on our Service, prior to the change becoming effective and update the "effective date" at the top of this Privacy Policy.
+          You are advised to review this Privacy Policy periodically for any changes. Changes to this Privacy Policy are effective when they are posted on this page.
+      </h6>
+
+      <h4>Contact Us</h4>
+      <h6> If you have any questions about this Privacy Policy, please contact us:
+          
+          By email: slathletecare@gmail.com</h6>
+      </div>    
+  </body>
+  </html>
