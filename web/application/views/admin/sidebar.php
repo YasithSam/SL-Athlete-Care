a08e7243dc872278abef765ade26392aecefa88c
YasithSam <yasith.sam7@gmail.com>
2021-10-18 14:00:24 +0530

    common controller

---
+++ web/application/views/admin/sidebar.php
@@ -0,0 +1,51 @@
+<input type="checkbox" id="sidebar-toggle">
+        <div class="sidebar">
+            <div class="sidebar-header">
+                <h3 class="brand">
+                    <span class="ti-unlink"></span>
+                    <span>SL Athlete Care</span>
+                </h3>
+                <label for="sidebar-toggle" class="ti-menu-alt"></label>
+            </div>
+
+            <div class="sidebar-menu">
+                <ul>
+                    <li>
+                        <a href="<?php echo BASEURL;?>/admin/">
+                            <span class="ti-dashboard"></span>
+                            <span>Dashboard</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="<?php echo BASEURL;?>/admin/casestudy">
+                            <span class="ti-files"></span>
+                            <span>Case Studies</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="<?php echo BASEURL;?>/admin/profile">
+                            <span class="ti-user"></span>
+                            <span>Profile</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="<?php echo BASEURL;?>/forumController/">
+                            <span class="ti-agenda"></span>
+                            <span>Forum</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="<?php echo BASEURL;?>/commonController/settings">
+                            <span class="ti-settings"></span>
+                            <span>Settings</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="<?php echo BASEURL;?>/accountController/logout">
+                            <span class="ti-power-off"></span>
+                            <span>Logout</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
