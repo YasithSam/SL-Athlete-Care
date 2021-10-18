768852b3342564ec3f0ad6e0a0a4eccb4ec7e190
YasithSam <yasith.sam7@gmail.com>
2021-10-18 14:09:28 +0530

    forum  model

--- web/application/views/admin/home.php
+++ web/application/views/admin/home.php
@@ -10,58 +10,8 @@
     </head>
 
     <body>
-        <input type="checkbox" id="sidebar-toggle">
-        <div class="sidebar">
-            <div class="sidebar-header">
-                <h3 class="brand">
-                    <span class="ti-unlink"></span>
-                    <span>SL Athlete Care</span>
-                </h3>
-                <label for="sidebar-toggle" class="ti-menu-alt"></label>
-            </div>
-
-            <div class="sidebar-menu">
-                <ul>
-                    <li>
-                        <a href="">
-                            <span class="ti-dashboard"></span>
-                            <span>Dashboard</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="">
-                            <span class="ti-files"></span>
-                            <span>Case Studies</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="">
-                            <span class="ti-user"></span>
-                            <span>Profile</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="">
-                            <span class="ti-agenda"></span>
-                            <span>Forum</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="">
-                            <span class="ti-settings"></span>
-                            <span>Settings</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="">
-                            <span class="ti-power-off"></span>
-                            <span>Logout</span>
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-
+        
+        <?php include "sidebar.php";?>
         <div class="main-content">
 
             <header>
