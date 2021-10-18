1f361053b70521ff76d05ccaf7086e338c02740d
YasithSam <yasith.sam7@gmail.com>
2021-10-18 14:10:00 +0530

    forum  model

--- web/application/views/doctor/bodyforum.php
+++ web/application/views/doctor/bodyforum.php
@@ -3,24 +3,30 @@
          
           <div class="title">Forum Injuries</div>
           <!--Injury card-->
-  
-            <div class="injury">
-              <i class="fas fa-user user"></i>
-              <div class="des1"><p>Name: </p><p>Sport: </p></div>
-              <div class="des2"><p>A.B.C.Perera</p><p>Swimming</p></div>
-              <div class="des3"></div>
-              <div class="des1-des11"><p>Injury: </p><p>Condition: </p></div>
-              <div class="des2"><p>Arm strain</p><p>Mild</p></div>
-              <div class="button">
-                <ul style="list-style: none;">
-                <li class="l1" style="padding-bottom: 5px;">Date: 01/02/2022</li>
-                <li class="l2"><a href="<?php echo BASEURL;?>/caseStudyController/item/1">View</a></li>
-                </ul>
-              </div>
-           
-          </div>
-         
-        
+          <?php if(!empty($data[1])): ?>
 
+             <?php foreach($data[1] as $item): ?>
+                  <div class="injury">
+                    <i class="fas fa-user user"></i>
+                    <div class="des1"><p>Name: </p><p>Injury: </p></div>
+                    <div class="des2"><p><?php echo ucwords($item->full_name); ?></p><p><?php echo ucwords($item->injury); ?></p></div>
+                    <div class="des3"></div>
+                    <div class="des1-des11"><p>Condition: </p><p>Visibility:</p></div>
+                    <?php $x=($item->doctor_id==0) ? "public" :"private"; ?>
+                    <div class="des2"><p><?php echo ucwords($item->con); ?></p> <p><?php echo $x?></p></div>
+                    <div class="button">
+                      <ul style="list-style: none;">
+                      <li class="l1" style="padding-bottom: 5px;">Date: 01/02/2022</li>
+                      <li class="l2"><a href="<?php echo BASEURL;?>/forumController/item/<?php echo $item->id; ?>">View</a></li>
+                      </ul>
+                    </div>
+                
+                </div>
+            <?php endforeach;?>
+        <?php else: ?>
+          <h1>No data </h1>
+        <?php endif; ?> 
+    
 
-        </div>
+          
+</div>
