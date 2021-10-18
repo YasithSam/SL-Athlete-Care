d7a33deff69501f59b1b59040eb5b49570bc18c8
YasithSam <yasith.sam7@gmail.com>
2021-10-18 14:01:32 +0530

    doctor

---
+++ web/application/views/doctor/athlete.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1">
+        <title></title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/lykmapipo/themify-icons@0.1.2/css/themify-icons.css">
+        <link href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css' rel='stylesheet'>
+        <?php linkCSS("assets/css/athete.css") ?>
+        <?php linkCSS("assets/css/athletemain.css") ?>
+    </head>
+
+<body>
+        <div class="main-content">
+            <header>
+                <div class="social-icons">
+                    <span class="ti-bell"></span>
+                    <div class="user-wrapper">
+                        <img src="avatar.jpg" width="40px" height="40px" alt="">
+                        <div>
+                            <h4>Doctor</h4>
+                            <small>Doctor</small>
+                        </div>
+                    </div>                
+                </div>
+            </header>
+<!--Body section-->
+            <main>  
+              <div class="home">
+              <!--Profile box-->
+                      <div class="profile box">
+                        <div class="title" style="justify-content: center;">Athlete Profile</div>
+                        <div class="img">
+                          <i class="fas fa-user prof"></i>
+                          <!--<div class="edit">
+                            <i class="fas fa-camera cam"></i>
+                          </div>-->
+                        </div>
+                        <div class="updt"><i class="fas fa-user user"></i>A. B. Chandimal Perera</div>
+                        <div class="updt"><i class="fas fa-user user"></i>24 years</div>
+                        <div class="updt"><i class="fas fa-map-marker-alt user"></i>Nugegoda</div>
+                        <div class="updt"><i class="fas fa-phone user"></i>078-2551255</div>
+                        <div class="updt"><i class="fas fa-at user"></i>abcperera@gmail.com</div>
+                        <div class="updt"><i class="fas fa-user-injured user"></i>Arm injury</div>
+                      </div>
+              <!--End of Profile box-->
+              
+                      <div class="right">
+              <!--Professional qualifications box-->
+                      <div class="about box">
+                        <div class="title">
+                          Sports Details
+                        </div>
+                        <!--card-->
+                        <div class="card">
+                          <i class="fas fa-running user"></i>
+                          <div class="qual">Gymnastic<br><p class="txt">Institute: National Youth Center &nbsp; &nbsp; &nbsp; &nbsp; Level: Beginner </p></div>
+                          </div>
+                        <!--card-->
+                        <div class="card">
+                          <i class="fas fa-running user"></i>
+                          <div class="qual">Water Pollo<br><p class="txt">Institute: National Youth Center &nbsp; &nbsp; &nbsp; &nbsp; Level: Beginner </p></div>
+                        </div>
+                      </div> 
+              <!--End of Professional qualifications box-->
+              
+              <!--Case studies box box-->
+                      <div class="about box">
+                        <div class="title">
+                          Health Details
+                        </div>
+                        <div class="health">
+                          <!--card-->
+                        <div class="card" style="margin-right: 20px; width: 50%;">
+                          <i class="fas fa-heartbeat user"></i>
+                          <div class="qual">Height<br><p class="txt">5' 3" </p></div>
+                        </div>
+                        <!--card-->
+                        <div class="card" style="width: 50%;">
+                          <i class="fas fa-heartbeat user"></i>
+                          <div class="qual">Weight<br><p class="txt">56 kg </p></div>
+                        </div>
+                      </div>
+                      <div class="health">
+                          <!--card-->
+                        <div class="card" style="margin-right: 20px; width: 50%;">
+                          <i class="fas fa-heartbeat user"></i>
+                          <div class="qual">BMI<br><p class="txt">50 </p></div>
+                        </div>
+                        <!--card-->
+                        <div class="card" style="width: 50%;">
+                          <i class="fas fa-heartbeat user"></i>
+                          <div class="qual">Blood pressure<br><p class="txt">something </p></div>
+                        </div>
+                      </div>
+                  </div>
+              <!--End of Case studies box box-->
+              
+              <!--Articles box-->
+                      <div class="about box">
+                        <div class="title">
+                          Other details
+                        </div>
+                          <!--card-->
+                        <div class="card">
+                          <i class="fas fa-info-circle user"></i>
+                          <div class="qual">Responsible person<br><p class="txt">Name: A.B.C. Silva <br>Telephone: 071-3232323 </p></div>
+                        </div>
+                      </div>
+              <!--End of Articles box-->
+              
+                    </div>      
+              </div>
+              
+                   
+            </main>
+        </div>
+    </body>
+</html>
