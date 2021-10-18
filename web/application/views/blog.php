b59753a752c10d9ec6ace2b217e8f1bf066d3ecd
YasithSam <yasith.sam7@gmail.com>
2021-10-18 14:00:31 +0530

    common controller

---
+++ web/application/views/blog.php
@@ -0,0 +1,521 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Blog layout</title>
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/lykmapipo/themify-icons@0.1.2/css/themify-icons.css">
+
+
+<script src="https://kit.fontawesome.com/4e3a3a38a1.js" crossorigin="anonymous"></script>
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/6.4.8/swiper-bundle.min.css"/>
+<?php linkCSS("assets/css/blog.css") ?>
+
+
+
+<body>
+
+<!--  <header>
+ 	<h1>Nav Bar</h1>
+ </header> -->
+
+ <div class="main-content">
+ <header>
+	<h2>SL Athlete Care</h2>
+<!--                 <div class="register-wrapper">
+		<a href="3.html" target="myiFrame"><button>Register User <span class="ti-plus"></span></button></a>
+	</div>
+-->
+	<div class="social-icons">
+		<span class="ti-bell"></span>
+		<div class="user-wrapper">
+			<img src="img/avatar.png" width="40px" height="40px" alt="">
+			<div>
+				<h4>User</h4>
+			</div>
+		</div>                
+	</div>
+</header> 
+
+<main>
+ <div class="row">
+ 	<div class="left-column">
+		 <h1>Latest News</h1>
+ 	  <div class="card1">
+		   <section>
+			<div class="testimonials-carousel-wrap">
+				<div class="listing-carousel-button listing-carousel-button-next"><i class="fa fa-caret-right" size="10px" style="color: #fff"></i></div>
+				<div class="listing-carousel-button listing-carousel-button-prev"><i class="fa fa-caret-left" style="color: #fff"></i></div>
+				<div class="testimonials-carousel">
+					<div class="swiper-container">
+						<div class="swiper-wrapper">
+							<!--first-->
+							<div class="swiper-slide">
+								<div class="testi-item">
+									<div class="testimonials-text-before"><i class="fa fa-quote-right"></i></div>
+									<div class="testimonials-text">
+										<div class="listing-rating">
+											<i class="fa fa-star"></i>
+											<i class="fa fa-star"></i>
+											<i class="fa fa-star"></i>
+											<i class="fa fa-star"></i>
+											<i class="fa fa-star"></i>
+										</div>
+										<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+										<a href="#" class="text-link"></a>
+										<div class="testimonials-avatar">
+											<h3>John Doe</h3>
+											<h4>Owner</h4>
+										</div>
+									</div>
+									<div class="testimonials-text-after"><i class="fa fa-quote-left"></i></div> 
+								</div>
+							</div>
+		
+							<!--second--->
+							<div class="swiper-slide">
+								<div class="testi-item">
+									<div class="testi-avatar"></div>
+									<div class="testimonials-text-before"><i class="fa fa-quote-right"></i></div>
+									<div class="testimonials-text">
+										<div class="listing-rating">
+											<i class="fa fa-star"></i>
+											<i class="fa fa-star"></i>
+											<i class="fa fa-star"></i>
+											<i class="fa fa-star"></i>
+											<i class="fa fa-star"></i>
+										</div>
+										<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+										<a href="#" class="text-link"></a>
+										<div class="testimonials-avatar">
+											<h3>Doe Boe</h3>
+											<h4>Director</h4>
+										</div>
+									</div>
+									<div class="testimonials-text-after"><i class="fa fa-quote-left"></i></div> 
+								</div>
+							</div>
+							<!--third-->
+		
+							<div class="swiper-slide">
+								<div class="testi-item">
+									<div class="testi-avatar"></div>
+									<div class="testimonials-text-before"><i class="fa fa-quote-right"></i></div>
+									<div class="testimonials-text">
+										<div class="listing-rating">
+											<i class="fa fa-star"></i>
+											<i class="fa fa-star"></i>
+											<i class="fa fa-star"></i>
+											<i class="fa fa-star"></i>
+											<i class="fa fa-star"></i>
+										</div>
+										<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+										<a href="#" class="text-link"></a>
+										<div class="testimonials-avatar">
+											<h3>Boe Doe</h3>
+											<h4>Developer</h4>
+										</div>
+									</div>
+									<div class="testimonials-text-after"><i class="fa fa-quote-left"></i></div> 
+								</div>
+							</div>
+		
+							<!--fourth-->
+							<div class="swiper-slide">
+								<div class="testi-item">
+									<div class="testi-avatar"></div>
+									<div class="testimonials-text-before"><i class="fa fa-quote-right"></i></div>
+									<div class="testimonials-text">
+										<div class="listing-rating">
+											<i class="fa fa-star"></i>
+											<i class="fa fa-star"></i>
+											<i class="fa fa-star"></i>
+											<i class="fa fa-star"></i>
+											<i class="fa fa-star"></i>
+										</div>
+										<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+										<a href="#" class="text-link"></a>
+										<div class="testimonials-avatar">
+											<h3>Doe John</h3>
+											<h4>Designer</h4>
+										</div>
+									</div>
+									<div class="testimonials-text-after"><i class="fa fa-quote-left"></i></div> 
+								</div>
+							</div>
+							<!--testi end-->
+		
+						</div>
+					</div>
+				</div>
+		
+				<div class="tc-pagination"></div>
+			</div>
+		
+
+		   </section>
+
+
+		
+ 	  </div>
+
+	   <div class="wrapper">
+			<div class="tabs_wrap">
+				<ul>
+					<li data-tabs="all" class="active">All</li>
+					<li data-tabs="online">Option1</li>
+					<li data-tabs="online">Option2</li>
+					<li data-tabs="offline">Option3</li>
+				</ul>
+			</div>
+		</div>
+		<?php 
+           
+        ?>
+		<div class="card3">
+
+			<div class="user-wrapper3">
+				<img src="img/avatar.png"  alt="">
+				<div class="user-name">
+					<h4>Kusal Mendis</h4>
+				</div>
+			</div> 	
+	
+			 <h3>Pathways through acute athlete care during training and major tournaments: a multi-national conceptualised process</h3>
+			 <h5>Dec 7, 2017</h5>
+			 <br>
+			
+			 
+			
+			 <!-- <img src="img/blog1.jpg" alt="blog "/> -->
+			 
+			 <p>There have been growing discussions across international societies since 2017 focused on athlete wellness and athlete care.</p>
+			 <br>
+			 <div class="decision-wrapper">
+				<a href=""><button class="button3">View</button></a>
+			</div>
+	
+			
+		</div>
+
+		   <div class="card3">
+
+			<div class="user-wrapper3">
+				<img src="img/avatar.png"  alt="">
+				<div class="user-name">
+					<h4>Kusal Mendis</h4>
+				</div>
+			</div> 	
+	
+			 <h3>Pathways through acute athlete care during training and major tournaments: a multi-national conceptualised process</h3>
+			 <h5>Dec 7, 2017</h5>
+			 <br>
+			
+			 
+			
+			 <!-- <img src="img/blog1.jpg" alt="blog "/> -->
+			 
+			 <p>There have been growing discussions across international societies since 2017 focused on athlete wellness and athlete care.</p>
+			 <br>
+			 <div class="decision-wrapper">
+				<a href=""><button class="button3">View</button></a>
+			</div>
+	
+			
+		   </div>
+		   <div class="card3">
+
+			<div class="user-wrapper3">
+				<img src="img/avatar.png"  alt="">
+				<div class="user-name">
+					<h4>Kusal Mendis</h4>
+				</div>
+			</div> 	
+	
+			 <h3>Pathways through acute athlete care during training and major tournaments: a multi-national conceptualised process</h3>
+			 <h5>Dec 7, 2017</h5>
+			 <br>
+			
+			 
+			
+			 <!-- <img src="img/blog1.jpg" alt="blog "/> -->
+			 
+			 <p>There have been growing discussions across international societies since 2017 focused on athlete wellness and athlete care.</p>
+			 <br>
+			 <div class="decision-wrapper">
+				<a href=""><button class="button3">View</button></a>
+			</div>
+	
+			
+		   </div>
+
+		   <div class="pagination">
+			<a href="#">&laquo;</a>
+			<a href="#" class="active">1</a>
+			<a href="#">2</a>
+			<a href="#">3</a>
+			<a href="#">4</a>
+			<a href="#">5</a>
+			<a href="#">6</a>
+			<a href="#">&raquo;</a>
+		  </div>
+		  
+
+	
+		
+ 	</div>
+
+	 
+
+
+
+ 	<div class="right-column">
+		 <h1>Forum Section</h1>
+ 		<div class="card2">
+			<div class="user-wrapper2">
+				<img src="img/avatar.png"  alt="">
+				<div class="user-name">
+					<h4>Kusal Mendis</h4>
+				</div>
+				<div class="sport-name">
+					<h6>Cricketer</h6>
+				</div>
+				<div class="injury">
+					<h6>Injury</h6>
+				</div>
+				<div class="condition">
+					<h6>Condition</h6>
+				</div>
+			</div> 	
+	
+			 <h4>Pathways through acute athlete care during training and major tournaments: a multi-national conceptualised process</h4>
+			 <h5>Dec 7, 2017</h5>
+
+			 <div class="decision-wrapper">
+				<a href=""><button class="button3">View</button></a>
+			</div>
+	
+				  
+ 		</div>
+		
+ 		<div class="card2">
+			<div class="user-wrapper2">
+				<img src="img/avatar.png"  alt="">
+				<div class="user-name">
+					<h4>Kusal Mendis</h4>
+				</div>
+				<div class="sport-name">
+					<h6>Cricketer</h6>
+				</div>
+				<div class="injury">
+					<h6>Injury</h6>
+				</div>
+				<div class="condition">
+					<h6>Condition</h6>
+				</div>
+			</div> 	
+	
+			 <h4>Pathways through acute athlete care during training and major tournaments: a multi-national conceptualised process</h4>
+			 <h5>Dec 7, 2017</h5>
+
+			 <div class="decision-wrapper">
+				<a href=""><button class="button3">View</button></a>
+			</div>
+	
+				  
+ 		</div>
+
+		 <div class="card2">
+			<div class="user-wrapper2">
+				<img src="img/avatar.png"  alt="">
+				<div class="user-name">
+					<h4>Kusal Mendis</h4>
+				</div>
+				<div class="sport-name">
+					<h6>Cricketer</h6>
+				</div>
+				<div class="injury">
+					<h6>Injury</h6>
+				</div>
+				<div class="condition">
+					<h6>Condition</h6>
+				</div>
+			</div> 	
+	
+			 <h4>Pathways through acute athlete care during training and major tournaments: a multi-national conceptualised process</h4>
+			 <h5>Dec 7, 2017</h5>
+
+			 <div class="decision-wrapper">
+				<a href=""><button class="button3">View</button></a>
+			</div>
+
+			
+	
+				  
+ 		</div>
+
+		 <div class="card2">
+			<div class="user-wrapper2">
+				<img src="img/avatar.png"  alt="">
+				<div class="user-name">
+					<h4>Kusal Mendis</h4>
+				</div>
+				<div class="sport-name">
+					<h6>Cricketer</h6>
+				</div>
+				<div class="injury">
+					<h6>Injury</h6>
+				</div>
+				<div class="condition">
+					<h6>Condition</h6>
+				</div>
+			</div> 	
+	
+			 <h4>Pathways through acute athlete care during training and major tournaments: a multi-national conceptualised process</h4>
+			 <h5>Dec 7, 2017</h5>
+
+			 <div class="decision-wrapper">
+				<a href=""><button class="button3">View</button></a>
+			</div>
+
+			
+	
+				  
+ 		</div>
+
+		 <div class="card2">
+			<div class="user-wrapper2">
+				<img src="img/avatar.png"  alt="">
+				<div class="user-name">
+					<h4>Kusal Mendis</h4>
+				</div>
+				<div class="sport-name">
+					<h6>Cricketer</h6>
+				</div>
+				<div class="injury">
+					<h6>Injury</h6>
+				</div>
+				<div class="condition">
+					<h6>Condition</h6>
+				</div>
+			</div> 	
+	
+			 <h4>Pathways through acute athlete care during training and major tournaments: a multi-national conceptualised process</h4>
+			 <h5>Dec 7, 2017</h5>
+
+			 <div class="decision-wrapper">
+				<a href=""><button class="button3">View</button></a>
+			</div>
+
+			
+	
+				  
+ 		</div>
+
+		 <div class="card2">
+			<div class="user-wrapper2">
+				<img src="img/avatar.png"  alt="">
+				<div class="user-name">
+					<h4>Kusal Mendis</h4>
+				</div>
+				<div class="sport-name">
+					<h6>Cricketer</h6>
+				</div>
+				<div class="injury">
+					<h6>Injury</h6>
+				</div>
+				<div class="condition">
+					<h6>Condition</h6>
+				</div>
+			</div> 	
+	
+			 <h4>Pathways through acute athlete care during training and major tournaments: a multi-national conceptualised process</h4>
+			 <h5>Dec 7, 2017</h5>
+
+			 <div class="decision-wrapper">
+				<a href=""><button class="button3">View</button></a>
+			</div>
+
+			
+	
+				  
+ 		</div>
+		 
+		 
+		 <div class="view">
+			<a href=""><button class="viewall">View All</button></a>
+		</div>
+
+
+ 	</div>
+ </div>
+
+</main>
+
+</div>
+
+    <!--footer starts-->
+    <div class="footer">
+
+        <div class="box-container">
+    
+            <div class="box">
+                <h3>about us</h3>
+                <p>The best sports specialists in the country, gathered together to provide the best services to the nation's athletes.</p>
+            </div>
+    
+            <div class="box">
+                <h3>quick links</h3>
+                <a href="#">home</a>
+                <a href="#">about us</a>
+                <a href="#">services</a>
+                <a href="#">reviews</a>
+                <a href="#">FAQ</a>
+                <a href="#">contact us</a>
+            </div>
+    
+            <div class="box">
+                <h3>stay connected</h3>
+                <div class="info">
+                    <i class="fab fa-facebook-f"></i>
+                    <a href="#">facebook</a>
+               </div>
+               <div class="info">
+                    <i class="fab fa-instagram"></i>
+                    <a href="#">instagram</a>
+               </div>
+               <div class="info">
+                    <i class="fab fa-twitter"></i>
+                    <a href="#">twitter</a>
+               </div>
+            </div>
+    
+            <div class="box">
+                <h3>contact info</h3>
+                <div class="info">
+                    <i class="fas fa-phone"></i>
+                    <p> +94-2304-345 <br> +94-7123-543 </p>
+                </div>
+                <div class="info">
+                    <i class="fas fa-envelope"></i>
+                    <p> example@gmail.com <br> example@gmail.com </p>
+                </div>
+                <div class="info">
+                    <i class="fas fa-map-marker-alt"></i>
+                    <p>Sports Medicine Association <br>Colombo <br>Sri Lanka </p>
+                </div>
+            </div>
+    
+        </div>
+    
+        <h1 class="credit"> &copy; copyright @ Sri Lanka Sports medicine Association </h1>
+    </div> 
+    <!--footer ends-->    
+
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/6.4.8/swiper-bundle.min.js"></script>
+	<script src="js/scripts.js"></script>
+  
+</body>
+</head>
