bd8ba28a100e72ff701dbc72f5a889565aba6934
YasithSam <yasith.sam7@gmail.com>
2021-10-18 14:08:48 +0530

    doctor model

--- web/application/models/doctorModel.php
+++ web/application/models/doctorModel.php
@@ -11,11 +11,37 @@ class doctorModel extends database
         }
 
     }   
-    // public function getForumItemns($id){
-    //     if($this->Query("SELECT * from athlete_reported_injury ")){
+     public function getForumItems($id){
+        if($this->Query("SELECT r.id,a.full_name,i.injury,r.con,r.doctor_id from athlete_reported_injury AS r inner join athlete_profile AS a On r.athlete_id=a.uuid inner join injury As i on i.id=r.injury_id where r.doctor_id=? || r.doctor_id=0",[$id])){
+            $x=$this->fetchall();
+            return $x;
 
-    //     }
-    // }
+        }
+    }
+    public function getProfile($id){
+        if($this->Query("SELECT uuid,full_name,province,sex,email,hospital from doctor_profile where uuid=?",[$id])){
+            $x=$this->fetch();
+            return $x;
+
+        }
+    }
+
+    public function getCaseStudy($id){
+        if($this->Query("SELECT c.title,a.full_name,i.injury from case_study AS c inner join athlete_profile AS a On r.athlete_id=a.uuid inner join injury As i on i.id=r.injury_id where c.doctor_id=?",[$id])){
+            $x=$this->fetchall();
+            return $x;
+
+        }
+    }
+
+    public function getArticle($id){
+        if($this->Query("SELECT r.id,a.full_name,i.injury,r.con,r.doctor_id from athlete_reported_injury AS r inner join athlete_profile AS a On r.athlete_id=a.uuid inner join injury As i on i.id=r.injury_id where r.doctor_id=? || r.doctor_id=0",[$id])){
+            $x=$this->fetchall();
+            return $x;
+
+        }
+    }
+
 
      // public function getUpdates($id){
     //     if($this->Query("SELECT * from athlete_reported_injury ")){
