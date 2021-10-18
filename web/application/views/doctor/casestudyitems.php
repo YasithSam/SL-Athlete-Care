7ea3e9c54e676d3ad86294ae0b677de5c8243711
YasithSam <yasith.sam7@gmail.com>
2021-10-18 14:10:33 +0530

    doctor view

--- web/application/views/doctor/casestudyitems.php
+++ web/application/views/doctor/casestudyitems.php
@@ -1,8 +1,8 @@
 <div class="menu">
             <!--<a id="showall">All</a>-->
-            <a class="single" target="1">Old</a>
-            <a class="single" target="2">Current</a>
-            <a class="single" target="3">Next</a>
+            <a class="single" target="1">Current</a>
+            <a class="single" target="2">Old</a>
+          
         </div>
         <section class="target_box">
             <div id="div1" class="target">
@@ -19,18 +19,12 @@
                 <div class="updt">Current</div>
                 <div class="updt">Current</div>
             </div>
-            <div id="div3" class="target">
-                <div class="updt">New</div>
-                <div class="updt">New</div>
-                <div class="updt">New</div>
-                <div class="updt">New</div>
-                <div class="updt">New</div>
-            </div>
+         
         </section>
         <script type="text/javascript">
-        jQuery('.single').click(function(){
-            jQuery('.target').hide();
-            jQuery('#div'+$(this).attr('target')).show();
-        });
+            jQuery('.single').click(function(){
+                jQuery('.target').hide();
+                jQuery('#div'+$(this).attr('target')).show();
+            });
         
         </script>
