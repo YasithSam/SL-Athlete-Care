f8a09d2523191b5c86f85c5342cc43728607f7a4
YasithSam <yasith.sam7@gmail.com>
2021-10-18 14:09:01 +0530

    forum  model

--- web/application/models/forumModel.php
+++ web/application/models/forumModel.php
@@ -3,14 +3,47 @@
 class forumModel extends database
 {
     public function getForumbyId($id){
-
-        if($this->Query("SELECT count(*) AS COUNT1 FROM athlete_profile union all SELECT count(*) AS COUNT1 FROM case_study where doctor_id=? union all SELECT count(*) AS COUNT1 FROM athlete_reported_injury where doctor_id=?",[$id,$id])){
-            $data = $this->fetchall();
+        if($this->Query("SELECT r.id,a.full_name,i.injury,r.con,r.doctor_id,r.date,r.description from athlete_reported_injury AS r inner join athlete_profile AS a On r.athlete_id=a.uuid inner join injury As i on i.id=r.injury_id where r.id=?",[$id])){
+            $data = $this->fetch();
             return $data;
 
         }
 
-    }  
+    } 
+    public function getForums($id){
+       
+        if($this->Query("SELECT injury_id from athlete_reported_injury where id=?",[$id])){
+            $data = $this->fetch();
+            
+        }
+        $x=$data->injury_id;
+        if($this->Query("SELECT r.id,a.full_name,i.injury,r.con,r.date from athlete_reported_injury AS r inner join athlete_profile AS a On r.athlete_id=a.uuid inner join injury As i on i.id=r.injury_id where r.injury_id=? && r.id!=?",[$x,$id])){
+            $datax = $this->fetchall();
+            return $datax;
+
+        }
+
+    } 
+    public function updateForumbyId($id,$status){
+      if($status=="Accepted"){
+        if($this->Query("UPDATE athlete_reported_injury set status='Accepted' where id=?",[$id])){
+
+        }
+        else{
+            echo "Record error";
+        }
+
+      }else{
+        if($this->Query("UPDATE athlete_reported_injury set status='Rejected' where id=?",[$id])){
+
+        }
+        else{
+            echo "Record error";
+        } 
+
+      }
+    } 
+
     public function access($id,$userId){
         // $q1=$this->Query("SELECT * from case_study where doctor_id=?",[$userId]);
         // $count1=$q1->rowCount();
