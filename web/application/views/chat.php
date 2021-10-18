80a75f61b3a7591d72cf1981fc77b0331d63f1b2
YasithSam <yasith.sam7@gmail.com>
2021-10-18 14:01:01 +0530

    common controller

---
+++ web/application/views/chat.php
@@ -0,0 +1,292 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1">
+        <title></title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/lykmapipo/themify-icons@0.1.2/css/themify-icons.css">
+        <?php linkCSS("assets/css/chat.css") ?>
+
+    </head>
+
+    <body>
+       <?php include "doctor/sidebar.php"?>
+
+        <div class="main-content">
+
+            <header>
+                <div class="register-wrapper">
+                    <a href="3.html" target="myiFrame"><button>Register User <span class="ti-plus"></span></button></a>
+                </div>
+
+                <div class="social-icons">
+                    <span class="ti-bell"></span>
+                    <div class="user-wrapper">
+                        <img src="avatar.jpg" width="40px" height="40px" alt="">
+                        <div>
+                            <h4>Doctor</h4>
+                            <small>Doctor</small>
+                        </div>
+                    </div>                
+                </div>
+            </header>
+
+            <main>
+            <div class="chat">
+                <div id="chat-container">
+                    <div id="search-container">
+                        <input type="text" placeholder="Search" />
+                    </div>
+                    <div id="conversation-list">
+            
+                        <div class="conversation active">
+                            <img src="../../web/public/assets/img/avatar.png" alt="Kusal Mendis" />
+                            <div class="title-text">
+                                Kusal Mendis
+                            </div>
+                            <div class="created-date">
+                                Apr 16
+                            </div>
+                            <div class="conversation-message">
+                                This is a message
+                            </div>
+                        </div>
+                                    
+                        <div class="conversation">
+                            <img src="../../web/public/assets/img/avatar.png" alt="Kusal Mendis" />
+                            <div class="title-text">
+                                Kusal Mendis
+                            </div>
+                            <div class="created-date">
+                                Apr 16
+                            </div>
+                            <div class="conversation-message">
+                                This is a message
+                            </div>
+                        </div>
+                        
+                        <div class="conversation">
+                            <img src="./images/avatar.png" alt="Kusal Mendis" />
+                            <div class="title-text">
+                                Kusal Mendis
+                            </div>
+                            <div class="created-date">
+                                Apr 16
+                            </div>
+                            <div class="conversation-message">
+                                This is a message
+                            </div>
+                        </div>
+                        
+                        <div class="conversation">
+                            <img src="./images/avatar.png" alt="Kusal Mendis" />
+                            <div class="title-text">
+                                Kusal Mendis
+                            </div>
+                            <div class="created-date">
+                                Apr 16
+                            </div>
+                            <div class="conversation-message">
+                                This is a message
+                            </div>
+                        </div>
+                        
+                        <div class="conversation">
+                            <img src="./images/avatar.png" alt="Kusal Mendis" />
+                            <div class="title-text">
+                                Kusal Mendis
+                            </div>
+                            <div class="created-date">
+                                Apr 16
+                            </div>
+                            <div class="conversation-message">
+                                This is a message
+                            </div>
+                        </div>
+                        
+                        <div class="conversation">
+                            <img src="./images/avatar.png" alt="Kusal Mendis" />
+                            <div class="title-text">
+                                Kusal Mendis
+                            </div>
+                            <div class="created-date">
+                                Apr 16
+                            </div>
+                            <div class="conversation-message">
+                                This is a message
+                            </div>
+                        </div>
+                        
+                        <div class="conversation">
+                            <img src="./images/avatar.png" alt="Kusal Mendis" />
+                            <div class="title-text">
+                                Kusal Mendis
+                            </div>
+                            <div class="created-date">
+                                Apr 16
+                            </div>
+                            <div class="conversation-message">
+                                This is a message
+                            </div>
+                        </div>
+                        
+                        <div class="conversation">
+                            <img src="./images/avatar.png" alt="Kusal Mendis" />
+                            <div class="title-text">
+                                Kusal Mendis
+                            </div>
+                            <div class="created-date">
+                                Apr 16
+                            </div>
+                            <div class="conversation-message">
+                                This is a message
+                            </div>
+                        </div>
+                        
+                        <div class="conversation">
+                            <img src="./images/avatar.png" alt="Kusal Mendis" />
+                            <div class="title-text">
+                                Kusal Mendis
+                            </div>
+                            <div class="created-date">
+                                Apr 16
+                            </div>
+                            <div class="conversation-message">
+                                This is a message
+                            </div>
+                        </div>
+                        
+                        <div class="conversation">
+                            <img src="./images/avatar.png" alt="Kusal Mendis" />
+                            <div class="title-text">
+                                Kusal Mendis
+                            </div>
+                            <div class="created-date">
+                                Apr 16
+                            </div>
+                            <div class="conversation-message">
+                                This is a message
+                            </div>
+                        </div>
+                        
+                        <div class="conversation">
+                            <img src="./images/avatar.png" alt="Kusal Mendis" />
+                            <div class="title-text">
+                                Kusal Mendis
+                            </div>
+                            <div class="created-date">
+                                Apr 16
+                            </div>
+                            <div class="conversation-message">
+                                This is a message
+                            </div>
+                        </div>
+                        
+                        <div class="conversation">
+                            <img src="./images/avatar.png" alt="Kusal Mendis" />
+                            <div class="title-text">
+                                Kusal Mendis
+                            </div>
+                            <div class="created-date">
+                                Apr 16
+                            </div>
+                            <div class="conversation-message">
+                                This is a message
+                            </div>
+                        </div>
+            
+            
+            
+                    </div>
+                    
+                    <div id="new-message-container">
+                        <a href="#">+</a>
+                    </div>
+                    <div id="chat-title">
+                        <span>Kusal Mendis</span>
+                        <img src="./images/trash.svg" alt="Delete Conversation" />
+                    </div>
+                    <div id="chat-message-list">
+                        <div class="message-row you-message">
+                            <div class="message-content">
+                                <div class="message-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</div>
+                                <div class="message-time">Apr 16</div>
+                            </div>
+                        </div>
+                        <div class="message-row other-message">
+                            <div class="message-content">
+                                <img src="./images/kusal-mendis.jpg" alt="Kusal Mendis" />
+                                <div class="message-text">What is Lorem Ipsum?</div>
+                                <div class="message-time">Apr 16</div>
+                            </div>
+                        </div>
+                        <div class="message-row you-message">
+                            <div class="message-content">
+                                <div class="message-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</div>
+                                <div class="message-time">Apr 16</div>
+                            </div>
+                        </div>
+                        <div class="message-row other-message">
+                            <div class="message-content">
+                                <img src="./images/kusal-mendis.jpg" alt="Kusal Mendis" />
+                                <div class="message-text">What is Lorem Ipsum?</div>
+                                <div class="message-time">Apr 16</div>
+                            </div>
+                        </div>
+                        <div class="message-row you-message">
+                            <div class="message-content">
+                                <div class="message-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</div>
+                                <div class="message-time">Apr 16</div>
+                            </div>
+                        </div>
+                        <div class="message-row other-message">
+                            <div class="message-content">
+                                <img src="./images/kusal-mendis.jpg" alt="Kusal Mendis" />
+                                <div class="message-text">What is Lorem Ipsum?</div>
+                                <div class="message-time">Apr 16</div>
+                            </div>
+                        </div>
+                        <div class="message-row you-message">
+                            <div class="message-content">
+                                <div class="message-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</div>
+                                <div class="message-time">Apr 16</div>
+                            </div>
+                        </div>
+                        <div class="message-row other-message">
+                            <div class="message-content">
+                                <img src="./images/kusal-mendis.jpg" alt="Kusal Mendis" />
+                                <div class="message-text">What is Lorem Ipsum?</div>
+                                <div class="message-time">Apr 16</div>
+                            </div>
+                        </div>
+                        <div class="message-row you-message">
+                            <div class="message-content">
+                                <div class="message-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</div>
+                                <div class="message-time">Apr 16</div>
+                            </div>
+                        </div>
+                        <div class="message-row other-message">
+                            <div class="message-content">
+                                <img src="./images/kusal-mendis.jpg" alt="Kusal Mendis" />
+                                <div class="message-text">What is Lorem Ipsum?</div>
+                                <div class="message-time">Apr 16</div>
+                            </div>
+                        </div>
+            
+            
+            
+                    </div>
+                    <div id="chat-form">
+                        <img src="./images/attachment.png" alt="Add Attachment" />
+                        <input type="text" placeholder="type a message...." />
+                    </div>
+                </div>
+
+            </div>
+            
+
+            </main>
+
+        </div>
+    </body>
+</html>
