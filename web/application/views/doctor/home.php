d13127e36bad92fcc3eecededc67f635d2589a97
YasithSam <yasith.sam7@gmail.com>
2021-10-18 14:10:45 +0530

    doctor view

--- web/application/views/doctor/home.php
+++ web/application/views/doctor/home.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <meta charset="UTF-8">
+    <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1">
         <title></title>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/lykmapipo/themify-icons@0.1.2/css/themify-icons.css">
-        <?php linkCSS("assets/css/doctordashboard.css") ?>
+        <link href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css' rel='stylesheet'>
         <?php linkCSS("assets/css/doctormain.css") ?>
+        <?php linkCSS("assets/css/doctordashboard.css") ?>
+        
 
     </head>
 
@@ -20,17 +22,17 @@
 
             <main>
             <section class="home-section">
-    <div class="home-content">
-      <?php include "top.php";?> 
-      <div class="forum">
-         <?php include "bodyforum.php";?>
-       
-        <div class="updates box">
-           <?php include "bodyupdates.php";?>
-           
-        </div>
-      </div>
-      </div>
+                <div class="home-content">
+                  <?php include "top.php";?> 
+                  <div class="forum">
+                    <?php include "bodyforum.php";?>
+                  
+                    <div class="updates box">
+                      <?php include "bodyupdates.php";?>
+                      
+                    </div>
+                  </div>
+                </div>
   </section>
 
              
