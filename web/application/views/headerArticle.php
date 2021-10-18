5e6fda9856f983aefea9911c380c501c8542c837
YasithSam <yasith.sam7@gmail.com>
2021-10-18 14:02:26 +0530

    profile

---
+++ web/application/views/headerArticle.php
@@ -0,0 +1,17 @@
+<div class="main-content">
+ <header>
+	<h2>SL Athlete Care</h2>
+<!--                 <div class="register-wrapper">
+		<a href="3.html" target="myiFrame"><button>Register User <span class="ti-plus"></span></button></a>
+	</div>
+-->
+	<div class="social-icons">
+		<span class="ti-bell"></span>
+		<div class="user-wrapper">
+			<img src="img/avatar.png" width="40px" height="40px" alt="">
+			<div>
+				<h4>User</h4>
+			</div>
+		</div>                
+	</div>
+</header>
