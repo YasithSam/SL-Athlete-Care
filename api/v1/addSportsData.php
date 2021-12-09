a68f83f515ba1ec2d706552147e3b023a20494c7
YasithSam <yasith.sam7@gmail.com>
2021-12-09 16:01:00 +0530

    user data api

--- api/v1/addSportsData.php
+++ api/v1/addSportsData.php
@@ -2,15 +2,14 @@
 <?php
 
 require_once '../../web/application/models/AthleteAPI.php';
-$id=$_REQUEST['sport_id'];
+$id=$_REQUEST['sport'];
 $a=$_REQUEST['athlete_id'];
 $i=$_REQUEST['institution'];
 $l=$_REQUEST['level'];
-$c=$_REQUEST['category'];
 $status;
 $db=new AthleteAPI();
-if(!(empty($id) || empty($a)||empty($i)||empty($l)||empty($c))){
-    $status=$db->addSportsData($id,$a,$i,$l,$c);
+if(!(empty($id) || empty($a)||empty($i)||empty($l))){
+    $status=$db->addSportsData($id,$a,$i,$l);
     echo json_encode($status);
 }
 else{
