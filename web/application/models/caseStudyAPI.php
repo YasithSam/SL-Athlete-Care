59dfe97c65e0f0b4557dbe8278a027c1c23d1ad4
YasithSam <yasith.sam7@gmail.com>
2021-12-20 16:26:12 +0530

    api update

--- web/application/models/caseStudyAPI.php
+++ web/application/models/caseStudyAPI.php
@@ -127,13 +127,15 @@ class caseStudyAPI extends database
 
     }
     public function getImages($id,$s){
+        // http://localhost/SL-Athlete-Care/web/public/assets/dbimages/
         $u=[];
-        if($this->Query("SELECT heading,description,datetime FROM case_study_records where type_id=? && case_id=? order datetime desc",[1,$id])){
+        if($this->Query("SELECT r.heading,r.description,a.link FROM case_study_records r inner join case_study_attachements a on r.id=a.case_study_record where r.case_id=? && r.state=?",[$id,$s])){
             if($this->rowCount() > 0 ){
                 $row = $this->fetchall();
                 $i=0;
                 foreach ($row as $obj)
                 {
+                    $obj->link="http://192.168.8.143/SL-Athlete-Care/web/public/assets/dbimages/".$obj->link;
                     $u[$i]=$obj;
                     $i++;
                      
@@ -148,37 +150,16 @@ class caseStudyAPI extends database
         return ['status'=>'ok','data'=>$u];
 
     }
-    public function getDiet($id){
+    public function getSchedules($id,$t){
         $w=[];
-        if($this->Query("SELECT d.id,d.title,d.description FROM schedule s inner join diet_schedule d on s.id=d.schedule_id where s.case_study_id =? && type=?",[$id,1])){
-            if($this->rowCount() > 0 ){
-                $row = $this->fetchall();
-                $i=0;
-                foreach ($row as $obj)
-                {
-                    $u[$i]=$obj;
-                    $i++;
-                     
-                }    
-       
-
-            } else {
-                return ['status' => 'n'];
-            }
-
-        }
-        return ['status'=>'ok','data'=>$w];
-
-    }
-    public function getWorkout($id){
         $d=[];
-        if($this->Query("SELECT w.id,w.title,w.description FROM schedule s inner join workout_schedule w on s.id=w.schedule_id where s.case_study_id =? && type=?",[$id,2])){
+        if($this->Query("SELECT d.id,d.title,d.description FROM schedule s inner join diet_schedule d on s.id=d.schedule_id where s.case_study_id =? && d.state=?",[$id,$t])){
             if($this->rowCount() > 0 ){
                 $row = $this->fetchall();
                 $i=0;
                 foreach ($row as $obj)
                 {
-                    $u[$i]=$obj;
+                    $d[$i]=$obj;
                     $i++;
                      
                 }    
@@ -189,12 +170,30 @@ class caseStudyAPI extends database
             }
 
         }
-        return ['status'=>'ok','data'=>$u];
+        if($this->Query("SELECT w.id,w.title,w.description FROM schedule s inner join workout_schedule w on s.id=w.schedule_id where s.case_study_id =? && w.state=?",[$id,$t])){
+            if($this->rowCount() > 0 ){
+                $row = $this->fetchall();
+                $i=0;
+                foreach ($row as $obj)
+                {
+                    $w[$i]=$obj;
+                    $i++;
+                     
+                }    
+       
+
+            } else {
+                return ['status' => 'n'];
+            }
+
+        }
+        return ['status'=>'ok','diet'=>$d,'workout'=>$w];
 
     }
+ 
     public function getWorkoutEvents($id){
         $e=[];
-        if($this->Query("SELECT title,reps,description FROM workout_events where workout_schdule_id =?",[$id])){
+        if($this->Query("SELECT title,reps,description,time FROM workout_events where workout_schdule_id =?",[$id])){
             if($this->rowCount() > 0 ){
                 $row = $this->fetchall();
                 $i=0;
@@ -212,6 +211,59 @@ class caseStudyAPI extends database
 
         }
         return ['status'=>'ok','data'=>$e];
+
+    }
+    public function uploadImage($c,$u,$data,$t,$d,$s){
+        $x=[$c,5,$u,$t,$d,$s];
+        if($this->Query("INSERT INTO case_study_records (case_id,type_id,recording_user_id,heading,description,state) VALUES (?,?,?,?,?,?)",$x)){
+            if($this->Query("SELECT id FROM case_study_records order by datetime desc Limit 1")){
+                $row=$this->fetch();
+                $id=$row->id;
+        
+                $link=strval($id).".jpeg";
+                if($this->Query("INSERT INTO case_study_attachements(case_study_record,link,type) values(?,?,?)",[$id,$link,5])){
+                    file_put_contents("/Applications/XAMPP/xamppfiles/htdocs/SL-Athlete-Care/web/public/assets/dbimages/".$link,base64_decode($data));
+                    return true;
+                }
+
+            }
+           
+        }else{
+            echo "noo";
+            return false;
+        }
+
+    }
+    public function addFeedback($f,$u,$c,$t,$s){
+      $x=[$t,$f,$c,$u,$s];
+      if($this->Query("INSERT INTO feedback (type,feedback,case_id,user_id,state) VALUES (?,?,?,?,?)",$x)){
+        return ['status'=>'ok'];
+       
+      }else{      
+        return ['status'=>'n'];
+      }
+
+
+    }
+    public function getFeedbacks($user,$id,$state){
+        $feedbacks=[];
+        if($this->Query("SELECT type,feedback,datetime,user_id from feedback where state=? && case_id=?",[$state,$id])){
+             if($this->rowCount()>0){
+                $row = $this->fetchall();
+                $i=0;
+                foreach ($row as $obj)
+                {
+                    if(strcmp($obj->user_id,$user)==0){
+                      $obj->type="0";
+                    }
+                    $feedbacks[$i]=$obj;
+                    $i++;
+                     
+                }    
+             }
+            return ['status'=>'ok','data'=>$feedbacks];
+        }
+        return ['status'=>'n'];
 
     }
     public function getDietEvents($id){
