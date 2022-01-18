9b425181604591f53a91d2369698019b58179324
YasithSam <yasith.sam7@gmail.com>
2022-01-18 19:09:01 +0530

    admin change

--- web/application/views/admin/home.php
+++ web/application/views/admin/home.php
@@ -9,107 +9,35 @@
         <?php linkCSS("assets/css/admin/adminmain.css") ?>
         <?php linkCSS("assets/css/admin/admin.css") ?>
         <?php linkCSS("assets/css/alert.css") ?>
-        <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-        
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     </head>
 
     <body>
       
         <?php include "sidebar.php"?>
 
-        <div class="main-content">
+<div class="main-content">
    
         <header>
-                <div class="register-wrapper">
-                    <a href="<?php echo BASEURL;?>/admin/register"><button>Register User <span class="ti-plus"></span></button></a>
+        <div class="register-wrapper">
+            <a href="<?php echo BASEURL;?>/admin/register"><button>Register User <span class="ti-plus"></span></button></a>
+        </div>
+        
+        <div class="social-icons">
+            <span class="ti-bell"></span>
+            <div class="user-wrapper">
+                <img src="../../web/public/assets/img/doctor.jpg" width="40px" height="40px" alt="">
+                <div>
+                    <h4>Administrator</h4>
+                    <small>Administrator</small>
                 </div>
-                
-                <div class="social-icons">
-                    <!-- <span class="ti-bell"></span> -->
-                    <div class="profile">
-                      <!-- <i class="fas fa-bell"></i> -->
-
-                      <div class="navbar_right">
-                        <div class="notifications">
-                          <div class="icon_wrap">
-                            <i class="far fa-bell"></i>
-                            <span>4</span>
-                          </div>
-                          
-                          <div class="notification_dd">
-                              <ul class="notification_ul">
-                                  <li class="starbucks success">
-                                      <div class="notify_icon">
-                                          <span class="icon"></span>  
-                                      </div>
-                                      <div class="notify_data">
-                                          <div class="title">
-                                              Lorem, ipsum dolor.  
-                                          </div>
-                                          <div class="sub_title">
-                                            Lorem ipsum dolor sit amet consectetur.
-                                          </div>
-                                      </div>
-                                      <div class="notify_status">
-                                          <p>Success</p>  
-                                      </div>
-                                  </li>  
-
-                                  <li class="show_all">
-                                      <p class="link">Show All Activities</p>
-                                  </li> 
-                              </ul>
-                          </div>
-                          
-                        </div>
-                      </div>
-
-                    </div>
-                      <button class="btn1" onclick="window.location.href='<?php echo BASEURL;?>/accountController/';"><i class="fa fa-user-circle" aria-hidden="true"></i></button>
-                    
-                    <div class="user-wrapper">
-                        <!-- <img src="../../web/public/assets/img/doctor.jpg" width="40px" height="40px" alt=""> -->
-                        <div>
-                            <h4><?php echo($data[1]->username)?></h4>
-                            <small><?php echo($data[1]->role)?></small>
-                        </div>
-                    </div>                
-                 </div>
-        </header>
+            </div>                
+        </div>
+</header>
    
     <!--Body section-->
     <main>
-
-    <div class="popup">
-      <div class="shadow"></div>
-        <div class="inner_popup">
-            <div class="notification_dd">
-                <ul class="notification_ul">
-                    <li class="title">
-                        <p>All Notifications</p>
-                        <p class="close"><i class="fas fa-times" aria-hidden="true"></i></p>
-                    </li> 
-                    <li class="starbucks success">
-                        <div class="notify_icon">
-                            <span class="icon"></span>  
-                        </div>
-                        <div class="notify_data">
-                            <div class="title">
-                                Lorem, ipsum dolor.  
-                            </div>
-                            <div class="sub_title">
-                              Lorem ipsum dolor sit amet consectetur.
-                          </div>
-                        </div>
-                        <div class="notify_status">
-                            <p>Success</p>  
-                        </div>
-                    </li>  
-              </ul>
-          </div>
-      </div>
-    </div>
-
           <div class="home-content">
           <?php $this->flash('docreg', 'alert alert-success') ?>
           <?php $this->flash('parareg', 'alert alert-success') ?>
@@ -119,21 +47,21 @@
                 <i class="fas fa-user-md icon"></i>
                 <div class="right-side">
                   <div class="box-topic">Total Athletes</div>
-                  <div class="number"><?php echo($data[0]['patients'])?></div>
+                  <div class="number"><?php print_r($data[0][0]->c1)?></div>
                 </div>
               </div>
               <div class="box">
                 <i class="fas fa-procedures icon"></i>
                 <div class="right-side">
                   <div class="box-topic">Total Case Studies</div>
-                  <div class="number"><?php echo($data[0]['casestudies'])?></div>
+                  <div class="number"><?php print_r($data[0][1]->c1)?></div>
                 </div>
               </div>
               <div class="box">
                 <i class="fas fa-users icon"></i>
                 <div class="right-side">
                   <div class="box-topic">Total Posts</div>
-                  <div class="number"><?php echo($data[0]['post'])?></div>
+                  <div class="number"><?php echo($data[0][2]->c1)?></div>
                 </div>
               </div>
             </div>
@@ -141,9 +69,10 @@
             <div class="charts">
               <div class="chart1 box">
                 <p>User movement</p>
-                <div class="graph">
-                    <img class="line" src="../../web/public/assets/img/line.jpeg" alt="">
+                <div id="chart-container">
+                  <canvas id="mycanvas"></canvas>
                 </div>
+
                 
               </div>
       
@@ -151,7 +80,9 @@
                     <div class="piechartbox">
                         <p>Athletes by sport</p>
                         
-                        <img class="pie" src="../../web/public/assets/img/pie.jpeg" alt="pie chart">
+                        <div class="chart-container pie-chart">
+								           <canvas id="pie_chart"></canvas>
+							         </div>
                     </div>
                     
               </div>
@@ -179,6 +110,9 @@
                 });
               }
               </script>
-               <?php linkJS("assets/js/notifications.js") ?>
+              
+              <?php linkJS("assets/js/users.js") ?>
+                  
+              <?php linkJS("assets/js/sports.js") ?>
     </body>
 </html>
