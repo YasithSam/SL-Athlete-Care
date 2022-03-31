829d66dc5ff1c03ac841a555ddf50270c9ddd5b5
YasithSam <62166536+YasithSam@users.noreply.github.com>
2022-04-01 00:03:08 +0530

    Update emailRejectCasestudy.php

--- api/v1/emailRejectCasestudy.php
+++ api/v1/emailRejectCasestudy.php
@@ -5,7 +5,7 @@ $db=new paramedicalModel();
 $id=$_REQUEST['id'];
 $data=$db->declineRequest($id);
 $email= new \SendGrid\Mail\Mail();
-$email->setFrom("elimoitsolutions@gmail.com", "Admin User");
+$email->setFrom(//, "Admin User");
 $email->setSubject("Your Case Study is declined!");
 $email->addTo($data->email, "Admin User - SL Athlete Care");
 $email->addContent("text/plain", "Your Case Study is declined");
@@ -13,7 +13,7 @@ $email->addContent(
     "text/html", "<strong>Dear Doctor,<br> Your Request for the Case Study " . $data->case_id . " is declined. <br></strong>"
     
 );
-$sendgrid = new \SendGrid('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');
+$sendgrid = new \SendGrid(//);
 try {
  $response = $sendgrid->send($email);
   header("location:" . BASEURL . "/paramedical/index?id=1",true,303);
